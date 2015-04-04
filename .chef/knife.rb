12ce231d37f865dab6ca80cb248ef5fdf6f77c4d
Tom Dooner <tom.dooner@brigade.com>
2015-04-03 20:36:20 -0700

    Add SSH Key ID

--- .chef/knife.rb
+++ .chef/knife.rb
@@ -1,3 +1,4 @@
 knife[:aws_access_key_id] = ENV['AWS_ACCESS_KEY_ID']
 knife[:aws_secret_access_key] = ENV['AWS_SECRET_ACCESS_KEY']
+knife[:aws_ssh_key_id] = 'brigade_ec2'
 knife[:region] = 'us-west-1'
